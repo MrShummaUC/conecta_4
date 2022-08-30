663c44fa46dc4f0f38e10700e0db7db0edb33983
Shumma <102603818+MrShummaUC@users.noreply.github.com>
2022-08-29 21:42:45 -0400

    Creacion página mantenimiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'index',
     loadChildren: () => import('./index/index.module').then( m => m.IndexPageModule)
   },
+  {
+    path: 'mantenimiento',
+    loadChildren: () => import('./mantenimiento/mantenimiento.module').then( m => m.MantenimientoPageModule)
+  },
 ];
 
 @NgModule({
